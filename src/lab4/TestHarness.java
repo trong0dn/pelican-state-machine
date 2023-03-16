c42a5710e281d65ed3f9b0fb3fbcb0325cade29e
trong0dn <55768917+trong0dn@users.noreply.github.com>
2023-03-16 13:39:53 -0400

    Update README and test

--- src/lab4/TestHarness.java
+++ src/lab4/TestHarness.java
@@ -9,10 +9,25 @@ public class TestHarness {
 	
 	public static void main(String[] args) throws InterruptedException {
 		Context context = new Context();
+		
+		// Press PEDESTRIAN_WAITING before TIMEOUT during vehiclesGreen
+		// vehiclesGreen -> vehiclesYellow
+		Thread.sleep(5000);
+		context.pedestrianWaiting();
+		
+		// Press PEDESTRIAN_WAITING before TIMEOUT during vehiclesYellow
+		// vehiclesYellow: No action expected
 		Thread.sleep(2000);
 		context.pedestrianWaiting();
-		Thread.sleep(10000);
+		
+		// Sleep at vehiclesGreenInt for 3000 ms
+		// vehiclesGreenInt -> vehiclesYellow
+		Thread.sleep(37000);	
 		context.pedestrianWaiting();
+		
+		// Sleep indefinitely at vehiclesGreenInt
+		Thread.sleep(50000);
+		System.out.print("> WAITING FOREVER FOR INTERRUPT");
 	}
 
 }
